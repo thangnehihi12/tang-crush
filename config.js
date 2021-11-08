f902ad68ba5145ec4a6b284b0779c719229cfddc
thangnehihi12 <93583721+thangnehihi12@users.noreply.github.com>
2021-11-08 16:47:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 Bconst CONFIG = {
-    introTitle: 'Bbi à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    introTitle: 'Chị Hìn à!',
+    introDesc: `Hihi iuuu chị nhiều. Bấm tiếp đi chị yêu`,
+    btnIntro: '<3',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên  😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Em Yêu Anh :33',
-    btnNo: 'Không, Thdiep à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    btnYes: 'Yêu Thdiep nhiều nhắmmm',
+    btnNo: 'hihi không nhaa !',
+    question:'Chị có yêu emm hong??',
+    btnReply: 'Gửi cho thdiep',
+    reply: 'Chị yêu em nhiều lắm <3',
+    mess: 'uuu là trời 🥰. Yêu chị nhiều nhiều 😘😘',
     messDesc: 'Yêu Bé Nhiều.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Dạaaaa lun <3',
     messLink: 'https://www.facebook.com/thang.diep06/' 
 }
