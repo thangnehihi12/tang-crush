c3c4d11da35133adcd228580c097f81e530829f2
thangnehihi12 <93583721+thangnehihi12@users.noreply.github.com>
2021-11-08 16:51:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 Bconst CONFIG = {
     introTitle: 'Chị Hìn à!',
-    introDesc: `Hihi iuuu chị nhiều. Bấm tiếp đi chị yêu`,
+    introDesc: `Hihi iuuu chị nhiều
+ Bấm tiếp đi chị yêu`,
     btnIntro: '<3',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên  😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Yêu Thdiep nhiều nhắmmm',
-    btnNo: 'hihi không nhaa !',
+    btnNo: 'hihi không nhaa ',
     question:'Chị có yêu emm hong??',
     btnReply: 'Gửi cho thdiep',
     reply: 'Chị yêu em nhiều lắm <3',
